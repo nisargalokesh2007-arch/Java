3af6c68a3298e227f8f376b9b36f044c479dfcf2
Nisarga L <nisargalokesh2007@gmail.com>
2026-08-19 21:53:12 +0530

    Add files via upload

---
+++ Even or Odd.java
@@ -0,0 +1,17 @@
+//checking even or add
+import java.util.Scanner;
+
+class EvenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        if (n % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+}
---
+++ Largest of 3 numbers.java
@@ -0,0 +1,21 @@
+//Largest of 3 numbers
+import java.util.Scanner;
+
+class Largest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter three numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if (a >= b && a >= c) {
+            System.out.println(a + " is largest");
+        } else if (b >= a && b >= c) {
+            System.out.println(b + " is largest");
+        } else {
+            System.out.println(c + " is largest");
+        }
+    }
+}
